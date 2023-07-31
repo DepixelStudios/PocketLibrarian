aae75685591f8470d3cbf795ead9b10fe7d7f20b
Srujan Ponnapalli <84602351+SrujanPonnapalli@users.noreply.github.com>
2023-07-31 01:28:20 -0400

    Update AllSubjectsPopup.kt

--- app/src/main/java/com/depixel/pocketlibrarian/AllSubjectsPopup.kt
+++ app/src/main/java/com/depixel/pocketlibrarian/AllSubjectsPopup.kt
@@ -14,20 +14,6 @@ class AllSubjectsPopup : AppCompatActivity() {
         overridePendingTransition(0, 0)
 
 
-//        val alpha = 100 //between 0-255
-//        val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
-//        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, alphaColor)
-//        colorAnimation.duration = 500 // milliseconds
-//        colorAnimation.addUpdateListener { animator ->
-//            popup_background.setBackgroundColor(animator.animatedValue as Int)
-//        }
-//        colorAnimation.start()
-
-//        moreSubjectsCard.alpha = 0f
-//        moreSubjectsCard.animate().alpha(1f).setDuration(500).setInterpolator(
-//            DecelerateInterpolator()
-//        ).start()
-
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         supportRequestWindowFeature(Window.FEATURE_SWIPE_TO_DISMISS)
 
